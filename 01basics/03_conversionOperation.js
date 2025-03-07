6fdacd247d654342d4d1c106476d823624d69a90
Deepak Kumar <deepakvns2027@gmail.com>
2025-03-07 07:37:04 +0000

    conversion of data types

---
+++ 01basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "abc";
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+
+console.log(typeof  valueInNumber);
+console.log(valueInNumber);
+
+//"33" => 33
+// "33abc" => NaN
+// true => 1;
+
+let isLoggedIn = "dee";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1=> true 0=> false
+// " "= false
+// "absc" => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
+
