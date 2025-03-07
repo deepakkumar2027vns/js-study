0cbbf689e9f6d8cda72208b19a51dd1665c83fb9
Deepak Kumar <deepakvns2027@gmail.com>
2025-03-07 07:12:56 +0000

    datatypes

---
+++ 01basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"Use strict"; // treat all js code as newer version
+
+// alert("hello");// 
+// alert(3+3); // error in node but in browser document mai kaaam krta hai , using node js not browser
+
+console.log(3+3); console.log("deepak"); // correct but not readeble
+
+
+// code readability should be heigh
+
+let name = "deepak" 
+let age = 18;
+let isloggedIn = false;
+let state = null;
+
+// number = 2 to pwer 53
+// bigint 
+// string =>""
+// boolean  => true / fasle
+// null => stand alone value
+// undefined => not assined
+// symbol =>  unique 
+
+// object
+
+console.log(typeof "hiii");  // string
+console.log(typeof age); // number
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
