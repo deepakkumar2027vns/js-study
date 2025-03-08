0bb49c51b0f351d7b965f2b00e7f83e0a11a62d4
Deepak Kumar <deepakvns2027@gmail.com>
2025-03-08 12:05:55 +0000

    datatypesummary

---
+++ datatypes-summary.js
@@ -0,0 +1,43 @@
+// # Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, symbol, BigiNT
+
+// JavaScript is a dynamic language and not static, which means that variables can hold values of different types during runtime. Unlike languages such as Typescript or Java, you don't need to declare the data type of a variable explicitly
+
+const score = 100;
+const scorevalue = 100.3;
+
+const isLoggedIn = false;
+let userEmail;
+
+const Id = Symbol("123");
+const anotherId = Symbol('123');
+
+console.log( Id === anotherId);
+
+const bigNumber = 66543456777654445667n; // bigInt
+
+
+
+
+
+//  # Reference (Non prinitive)
+
+// Array, Objects, Function
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22
+}
+
+const myFunction = function(){
+    console.log("hello world");
+}
+
+console.log(typeof bigNumber);
+console.log(typeof(null)); // object
+
+console.log(typeof myFunction); // function object
+console.log(typeof(Id)); // symbol
+
